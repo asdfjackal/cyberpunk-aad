c1010ce2fefda75a22962cae54540b5ba9d8b2c6
Aleksandr White <aleksandr@asdfjackal.com>
2021-10-28 19:31:02 -0400

    Migrated client and server code into DM and player apps.

--- src/pages/DMApp.svelte
+++ src/pages/DMApp.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
+  import Peer, { DataConnection } from "peerjs";
+
+  import { v4 as uuid4 } from "uuid";
   import CleanLocal from "../components/CleanLocal.svelte";
   import Roller from "../components/Roller.svelte";
   import RollForm from "../components/RollForm.svelte";
   import Tracker from "../components/Tracker.svelte";
-  import WebRtcTest from "../components/WebRTCHost.svelte";
+
+  const roomCode = uuid4();
+  let connected = false;
+  const peer = new Peer(roomCode, { debug: 2 });
+  let conn: DataConnection;
+  let conns: DataConnection[] = [];
+
+  let peers = 0;
+  $: peers = conns.length;
+
+  peer.on("connection", (c: DataConnection) => {
+    c.on("open", () => {
+      connected = true;
+    });
+    conn = c;
+    conns.push(c);
+    conns = conns;
+  });
+
+  function cleanConns() {
+    conns = conns.filter((c) => c.open === true);
+  }
+
+  function handleClick() {
+    cleanConns();
+    conns.forEach((c) => {
+      c.send("test");
+    });
+  }
 </script>
 
 <main>
@@ -19,5 +50,7 @@
   <Tracker />
   <RollForm />
   <br />
-  <WebRtcTest />
+  <p>{peers} Peers Connected</p>
+  <button on:click={handleClick}>Send Message</button>
+  <a href="localhost:5000/room/{roomCode}">{roomCode}</a>
 </main>
--- src/pages/PlayerApp.svelte
+++ src/pages/PlayerApp.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-  import WebRtcClient from "../components/WebRTCClient.svelte";
-
+  import Peer, { DataConnection } from "peerjs";
   export let id: string;
+  let connected = false;
+  let lastData = "No data yet";
+  const peer = new Peer(null, { debug: 2 });
+  let conn: DataConnection;
+  peer.on("open", () => {
+    conn = peer.connect(id, { reliable: true });
+    conn.on("open", () => {
+      connected = true;
+      conn.on("data", (data) => {
+        lastData = data;
+      });
+    });
+  });
+  function handleClick() {}
 </script>
 
-<WebRtcClient roomCode={id} />
+<p>{lastData}</p>
